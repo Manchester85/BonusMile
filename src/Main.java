21a8717ba0123fefb86a12db7acb710303fc6e89
Maria Vostrikova <vostrikova.maria2017@yandex.ru>
2021-08-21 22:17:14 +0300

    reformat code added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
-        public static void main(String[] args) {
-            BonusMilesService service = new BonusMilesService();
-            int cost = 10_000;
-            int bonusMile = service.calculate(10_000, 20);
-            System.out.println(bonusMile);
-        }
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int cost = 10_000;
+        int bonusMile = service.calculate(10_000, 20);
+        System.out.println(bonusMile);
     }
+}
 
 
 
